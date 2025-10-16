4bcc68b1ae46282b3631fbecd105b0af4fac1a3d
Collins Kibet <kollcibe05@gmail.com>
2025-10-16 07:03:22 +0300

    solved commit

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,17 @@
+import React from "react"
+
 // Code EyesOnMe Component Here
+
+function EyesOnMe () {
+    return <button onBlur={handleBlur} onFocus={handleFocus}>Eyes on me</button>
+}
+
+function handleFocus () {
+    console.log("Good!")
+}
+
+function handleBlur () {
+    console.log("Hey! Eyes on me!")
+}
+
+export default EyesOnMe;
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,9 +1,21 @@
+import React from "react"
 // Code Keypad Component Here
 
 function Keypad (){
     return (
-        <div></div>
+        <div onChange={handleInput}>
+        <input 
+        type="password"
+        name="password"
+        placeholder="Enter your Password"
+        />
+        </div>
     )
+}
+
+function handleInput (event) {
+console.log("Entering password...")
+
 }
 
 export default Keypad;
